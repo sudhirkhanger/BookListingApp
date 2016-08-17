5ec66f62bd34aa961584ccc92d241722dbd3ad48
Sudhir Khanger <sudhir@sudhirkhanger.com>
2016-08-17 15:03:39 +0530

    first commit

---
+++ app/src/main/java/com/sudhirkhanger/app/booklistingapp/BookListingActivityFragment.java
@@ -0,0 +1,22 @@
+package com.sudhirkhanger.app.booklistingapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class BookListingActivityFragment extends Fragment {
+
+    public BookListingActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_book_listing, container, false);
+    }
+}
